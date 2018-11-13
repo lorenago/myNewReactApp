981bb6757a62c5a6331c02b8cb660656fd16b764
Lorena González <lore_383@hotmail.com>
2018-11-13 23:30:27 +0100

    Updates

---
+++ precache-manifest.08cddb65a65b6ca7a8261742d6cadd06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/myNewReactApp/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "2a9d9d6129c2973c85d0",
+    "url": "/myNewReactApp/static/js/runtime~main.2a9d9d61.js"
+  },
+  {
+    "revision": "1c002b5e4ab1317ceebf",
+    "url": "/myNewReactApp/static/js/main.1c002b5e.chunk.js"
+  },
+  {
+    "revision": "54072b47299df94304a5",
+    "url": "/myNewReactApp/static/js/1.54072b47.chunk.js"
+  },
+  {
+    "revision": "1c002b5e4ab1317ceebf",
+    "url": "/myNewReactApp/static/css/main.b6691222.chunk.css"
+  },
+  {
+    "revision": "5677fc0328c3c45cad9b8afa0a8cdd51",
+    "url": "/myNewReactApp/index.html"
+  }
+];
